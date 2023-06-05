c6af954a29e46e5e6fd0da12a86439ac925a44d0
angel <jjjk>
2023-06-05 23:10:01 +0200

    Diseño completado

--- src/main/java/com/example/ejercicio2examen2evdi/VentaBilletesController.java
+++ src/main/java/com/example/ejercicio2examen2evdi/VentaBilletesController.java
@@ -1,14 +1,46 @@
 package com.example.ejercicio2examen2evdi;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class VentaBilletesController {
     @FXML
-    private Label welcomeText;
+    private TextField tfNombre;
+    @FXML
+    private TextField tfCiudadDestino;
+    @FXML
+    private TextField tfTarifaTotal;
+    @FXML
+    private Button btGuardar;
+    @FXML
+    private Button btMostrar;
+    @FXML
+    private Button btCancelar;
+    @FXML
+    private Button btCalcular;
+    @FXML
+    private CheckBox checkDesayuno;
+    @FXML
+    private CheckBox checkCena;
+    @FXML
+    private CheckBox checkComida;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onGuardarClick() {
+    }
+
+    @FXML
+    protected void onMostrarClick() {
+    }
+
+    @FXML
+    protected void onCancelarClick() {
+    }
+
+    @FXML
+    protected void onCalcularClick() {
     }
 }
